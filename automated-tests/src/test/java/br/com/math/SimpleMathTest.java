2f70219d136f73527fba6c284c6430a3e1fd0f25
GeorgeSalu <george.salu10@gmail.com>
2023-07-28 13:52:56 -0300

    primeiros passos com junit 5 - implementando teste automatizados para as demais operacoes da matematica

--- automated-tests/src/test/java/br/com/math/SimpleMathTest.java
+++ automated-tests/src/test/java/br/com/math/SimpleMathTest.java
@@ -1,8 +1,6 @@
 package br.com.math;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,8 +16,65 @@ class SimpleMathTest {
 		double expected = 8.2D;
 		
 		assertEquals(expected, actual, () -> "6.2+2 did not produce 8.2");
-		assertNotEquals(9.2, actual);
-		assertNotNull(actual);
+	}
+	
+	@Test
+	void testSubtraction() {
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		
+		Double actual = math.subtraction(firstNumber, secondNumber);
+		double expected = 4.2D;
+		
+		assertEquals(expected, actual, () -> "6.2-2 did not produce 8.2");
+	}
+	
+	@Test
+	void testMultiplication() {
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		
+		Double actual = math.multiplication(firstNumber, secondNumber);
+		double expected = 12.4D;
+		
+		assertEquals(expected, actual, () -> "6.2*2 did not produce 8.2");
+	}
+	
+	@Test
+	void testDivision() {
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		
+		Double actual = math.division(firstNumber, secondNumber);
+		double expected = 3.1D;
+		
+		assertEquals(expected, actual, () -> "6.2/2 did not produce 8.2");
+	}
+	
+	@Test
+	void testMean() {
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		
+		Double actual = math.mean(firstNumber, secondNumber);
+		double expected = 4.1D;
+		
+		assertEquals(expected, actual, () -> "(6.2+2)/2 did not produce 8.2");
+	}
+	
+	@Test
+	void testSquareRoot() {
+		SimpleMath math = new SimpleMath();
+		double number = 81D;
+		double expected = 9D;
+		
+		Double actual = math.squaredRoot(number);
+		
+		assertEquals(expected, actual, () -> "square root of 81 did not produce 8.2");
 	}
 
 }
