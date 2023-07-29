9fe33ad94e4a9495a3adcd93a7b28f28e3ba449e
GeorgeSalu <george.salu10@gmail.com>
2023-07-29 10:02:17 -0300

    Test Driven Development (TDD) - criando o teste de criação de persons

---
+++ automated-tests/src/test/java/br/com/PersonServiceTest.java
@@ -0,0 +1,19 @@
+package br.com;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PersonServiceTest {
+
+	@Test
+	@DisplayName("When create a person with sucess should return a person object")
+	void testCreatePerson_WhenSucess_ShouldReturnPersonObject() {
+		// Given / Arrange
+		
+		// When / Act
+		
+		// Then / Assert
+		
+	}
+	
+}
