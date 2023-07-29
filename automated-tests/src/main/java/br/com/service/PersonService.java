be15a022470a8c3158c7b45cd1d7aac8e2b67202
GeorgeSalu <george.salu10@gmail.com>
2023-07-29 10:13:04 -0300

    Test Driven Development (TDD) - criando o PersonService

---
+++ automated-tests/src/main/java/br/com/service/PersonService.java
@@ -0,0 +1,5 @@
+package br.com.service;
+
+public class PersonService implements IPersonService {
+
+}
--- automated-tests/src/test/java/br/com/PersonServiceTest.java
+++ automated-tests/src/test/java/br/com/PersonServiceTest.java
@@ -3,12 +3,16 @@ package br.com;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import br.com.service.IPersonService;
+import br.com.service.PersonService;
+
 public class PersonServiceTest {
 
 	@Test
 	@DisplayName("When create a person with sucess should return a person object")
 	void testCreatePerson_WhenSucess_ShouldReturnPersonObject() {
 		// Given / Arrange
+		IPersonService service = new PersonService();
 		
 		// When / Act
 		
