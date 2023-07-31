763e96c28131e2e5e736e17015a8c6642be82d82
GeorgeSalu <george.salu10@gmail.com>
2023-07-31 15:22:21 -0300

    conceitos avancados de mockito - conhecendo as principais annotations do mockito

---
+++ automated-tests/src/test/java/br/com/mockito/CourseBusinessInjectMocksTest.java
@@ -0,0 +1,108 @@
+package br.com.mockito;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.business.CourseBusiness;
+import br.com.service.CourseService;
+
+@ExtendWith(MockitoExtension.class)
+class CourseBusinessInjectMocksTest {
+
+	@Mock
+	CourseService mockService;
+
+	@InjectMocks
+	CourseBusiness business;
+	List<String> courses;
+	
+	@Captor
+	ArgumentCaptor<String> argumentsCaptor;
+	
+	@BeforeEach
+	void setup() {
+		// Given / Arrange
+		// business = new CourseBusiness(mockService);
+		
+		courses = Arrays.asList(
+	        "REST API's RESTFul do 0 à Azure com ASP.NET Core 5 e Docker",
+	        "Agile Desmistificado com Scrum, XP, Kanban e Trello",
+	        "Spotify Engineering Culture Desmistificado",
+	        "REST API's RESTFul do 0 à AWS com Spring Boot 3 Java e Docker",
+	        "Docker do Zero à Maestria - Contêinerização Desmistificada",
+	        "Docker para Amazon AWS Implante Apps Java e .NET com Travis CI",
+	        "Microsserviços do 0 com Spring Cloud, Spring Boot e Docker",
+	        "Arquitetura de Microsserviços do 0 com ASP.NET, .NET 6 e C#",
+	        "REST API's RESTFul do 0 à AWS com Spring Boot 3 Kotlin e Docker",
+	        "Kotlin para DEV's Java: Aprenda a Linguagem Padrão do Android",
+	        "Microsserviços do 0 com Spring Cloud, Kotlin e Docker"
+	    );
+	}
+	
+	@Test
+	void testCoursesRelatedToSpring_When_usingAStub() {
+		// Given / Arrange
+		given(mockService.retrieveCourse("Leandro")).willReturn(courses);
+		
+		// When / Act
+		var filteredCourses = business.retrieveCoursesRelatedToSpring("Leandro");
+		
+		// Then / Assert
+		assertThat(filteredCourses.size(), is(4));
+	}
+	
+	@Test
+	@DisplayName("Delete courses not releted to spring using mockito sould call method")
+	void testDeleteCoursesNotReletedToSpring_Using_mockito_verified() {
+		// Given / Arrange
+		given(mockService.retrieveCourse("Leandro")).willReturn(courses);
+		// When / Act
+		business.deleteCoursesNotRelatedToSpring("Leandro");
+		
+		// Then / Assert
+		verify(mockService, times(1)).deleteCourse("Agile Desmistificado com Scrum, XP, Kanban e Trello");
+		verify(mockService, never()).deleteCourse("REST API's RESTFul do 0 à AWS com Spring Boot 3 Java e Docker");
+	}
+	
+	@Test
+	@DisplayName("Delete courses not releted to spring using arguments captors sould call method V2")
+	void testDeleteCoursesNotReletedToSpring_Using_mockito_verifiedV2() {
+		// Given / Arrange
+		courses = Arrays.asList(
+		        "Agile Desmistificado com Scrum, XP, Kanban e Trello",
+		        "REST API's RESTFul do 0 à AWS com Spring Boot 3 Java e Docker"
+		);
+		
+		given(mockService.retrieveCourse("Leandro")).willReturn(courses);
+
+		
+		String agileCourse = "Agile Desmistificado com Scrum, XP, Kanban e Trello";
+
+		// When / Act
+		business.deleteCoursesNotRelatedToSpring("Leandro");
+		
+		// Then / Assert
+		
+		then(mockService).should().deleteCourse(argumentsCaptor.capture());
+		assertThat(argumentsCaptor.getValue(), is(agileCourse));
+	}
+
+}
