c43bbbe4bc0f6efd6f2188f4e0c708c64a4a820c
GeorgeSalu <george.salu10@gmail.com>
2023-07-31 19:03:13 -0300

    conceitos avancados de mockito - mockando um LocalDateTime

--- automated-tests/src/test/java/br/com/mockito/services/OrderServiceTest.java
+++ automated-tests/src/test/java/br/com/mockito/services/OrderServiceTest.java
@@ -2,6 +2,7 @@ package br.com.mockito.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ class OrderServiceTest {
 	
     private final OrderService cut = new OrderService();
     private final UUID defaultUuid = UUID.fromString("8d8b30e3-de52-4f1c-a71c-9905a8043dac");
+    private final LocalDateTime defaultLocalDateTime = LocalDateTime.of(2023, 7, 4, 12, 0);
 
     @Test
     void shouldIncludeRandomOrderIdWhenNoParentOrderExists() {
@@ -22,6 +24,16 @@ class OrderServiceTest {
             Order result = cut.createOrder("MacBook Pro", 2L, null);
 
             assertEquals("8d8b30e3-de52-4f1c-a71c-9905a8043dac", result.getId());
+        }
+    }
+
+    @Test
+    void shouldIncludeCurrentTimeWhenCreatingANewOrder() {
+        try (MockedStatic<LocalDateTime> mockedLocalDateTime = Mockito.mockStatic(LocalDateTime.class)) {
+            mockedLocalDateTime.when(LocalDateTime::now).thenReturn(defaultLocalDateTime);
+
+            Order result = cut.createOrder("MacBook Pro", 2L, "42");
+            assertEquals(defaultLocalDateTime, result.getCreationDate());
         }
     }
 
