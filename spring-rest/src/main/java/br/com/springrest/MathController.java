71e4a77740d48ebba5e473ae388bb70b41e9aee9
GeorgeSalu <george.salu10@gmail.com>
2023-08-02 10:27:01 -0300

    nivelamento spring rest - definindo a logica da aplicação

--- spring-rest/src/main/java/br/com/springrest/MathController.java
+++ spring-rest/src/main/java/br/com/springrest/MathController.java
@@ -9,8 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class MathController {
 
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double greeting(@PathVariable(value = "numberOne") String numberOne,@PathVariable(value = "numberTwo") String numberTwo) {
-		return 1D;
+	public Double greeting(@PathVariable(value = "numberOne") String numberOne,@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+		if(isNumeric(numberOne) || isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+		
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+
+	private Double convertToDouble(String numberOne) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private boolean isNumeric(String numberOne) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
