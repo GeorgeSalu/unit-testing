cf7c90d90dd97c2c3ef7be8b613f376d41285e4d
GeorgeSalu <george.salu10@gmail.com>
2023-08-02 12:04:55 -0300

    nivelamento spring rest - os verbos http post, put e delete

--- spring-rest/src/main/java/br/com/springrest/services/PersonServices.java
+++ spring-rest/src/main/java/br/com/springrest/services/PersonServices.java
@@ -11,39 +11,63 @@ import br.com.springrest.model.Person;
 
 @Service
 public class PersonServices {
-
-	private final AtomicLong counter = new AtomicLong();
-	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
+	private final AtomicLong counter = new AtomicLong();
+	
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
 	public List<Person> findAll() {
+
+		logger.info("Finding all people!");
+		
 		List<Person> persons = new ArrayList<>();
 		for (int i = 0; i < 8; i++) {
 			Person person = mockPerson(i);
 			persons.add(person);
 		}
-		
 		return persons;
-	}
-	
-	private Person mockPerson(int i) {
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Leandro "+i);
-		person.setLastName("Costa "+i);
-		person.setAddress("Minas "+i);
-		person.setGender("Male "+i);
-		return person;
 	}
 
 	public Person findById(String id) {
-		logger.info("Finding one person");
+		
+		logger.info("Finding one person!");
+		
 		Person person = new Person();
 		person.setId(counter.incrementAndGet());
 		person.setFirstName("Leandro");
 		person.setLastName("Costa");
-		person.setAddress("Minas");
+		person.setAddress("Uberlândia - Minas Gerais - Brasil");
 		person.setGender("Male");
 		return person;
 	}
 	
+	public Person create(Person person) {
+
+		logger.info("Creating one person!");
+		
+		return person;
+	}
+	
+	public Person update(Person person) {
+		
+		logger.info("Updating one person!");
+		
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+		logger.info("Deleting one person!");
+	}
+	
+	private Person mockPerson(int i) {
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in Brasil " + i);
+		person.setGender("Male");
+		return person;
+	}
 }
