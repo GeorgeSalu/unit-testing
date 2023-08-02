6b05d6486b1ab45b25e6e23bc409a66363f81d69
GeorgeSalu <george.salu10@gmail.com>
2023-08-02 11:55:50 -0300

    nivelamento spring rest - retornando a lista de objetos em json

--- spring-rest/src/main/java/br/com/springrest/services/PersonServices.java
+++ spring-rest/src/main/java/br/com/springrest/services/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.springrest.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,26 @@ public class PersonServices {
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+	
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Leandro "+i);
+		person.setLastName("Costa "+i);
+		person.setAddress("Minas "+i);
+		person.setGender("Male "+i);
+		return person;
+	}
+
 	public Person findById(String id) {
 		logger.info("Finding one person");
 		Person person = new Person();
