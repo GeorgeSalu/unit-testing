c6a684a94bfb3d9b244c1793295f63129d8de1a3
GeorgeSalu <george.salu10@gmail.com>
2023-08-05 10:28:08 -0300

    nivelamento spring rest - substituindo os mocks do service pelo aceso ao repositorio

--- spring-rest/src/main/java/br/com/springrest/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ spring-rest/src/main/java/br/com/springrest/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.springrest.exceptions.ExceptionResponse;
-import br.com.springrest.exceptions.UnsupportedMathOperationException;
+import br.com.springrest.exceptions.ResourceNotFoundException;
 
 @RestController
 @ControllerAdvice
@@ -23,10 +23,10 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@ExceptionHandler(UnsupportedMathOperationException.class)
-	public final ResponseEntity<ExceptionResponse> handlerBadRequestExceptions(Exception ex,WebRequest request) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handlerNotFoundExceptions(Exception ex,WebRequest request) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 	
 }
