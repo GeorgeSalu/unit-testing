938da77714b34bb116845d452c6947dd31f201e9
GeorgeSalu <george.salu10@gmail.com>
2023-08-08 13:25:28 -0300

    projeto junit5-curso - outas assertivas

--- junit5-curso/src/test/java/CalculadoraTest.java
+++ junit5-curso/src/test/java/CalculadoraTest.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,4 +12,23 @@ public class CalculadoraTest {
 		Assertions.assertTrue(calculadora.soma(2, 7) == 9);
 		Assertions.assertEquals(5, calculadora.soma(2, 3));
 	}
+	
+	@Test
+	public void assertivas() {
+		Assertions.assertEquals("Casa", "Casa");
+		Assertions.assertNotEquals("Casa", "casa");
+		Assertions.assertTrue("Casa".equalsIgnoreCase("CASA"));
+		Assertions.assertTrue("Casa".endsWith("sa"));
+		Assertions.assertTrue("Casa".startsWith("Ca"));
+		
+		List<String> s1 = new ArrayList<>();
+		List<String> s2 = new ArrayList<>();
+		List<String> s3 = null;
+		
+		Assertions.assertEquals(s1, s2);
+		Assertions.assertSame(s2, s2);
+		Assertions.assertNull(s3);
+		Assertions.assertNotNull(s1);
+	}
+	
 }
