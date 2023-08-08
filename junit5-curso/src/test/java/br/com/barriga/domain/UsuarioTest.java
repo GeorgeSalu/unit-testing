b025593df9698b06eee87947f8e93c77a3d02710
GeorgeSalu <george.salu10@gmail.com>
2023-08-08 20:21:41 -0300

    projeto junit5-curso - deve criar um usuario valido

---
+++ junit5-curso/src/test/java/br/com/barriga/domain/UsuarioTest.java
@@ -0,0 +1,20 @@
+package br.com.barriga.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class UsuarioTest {
+
+	@Test
+	void deveCriarUsuarioValido() {
+		
+		Usuario usuario = new Usuario(1l, "usuario valido", "user@gmail.com", "1234");
+		
+		assertEquals(1l, usuario.id());
+		assertEquals("usuario valido", usuario.nome());
+		assertEquals("user@gmail.com", usuario.email());
+		assertEquals("1234", usuario.senha());
+	}
+
+}
