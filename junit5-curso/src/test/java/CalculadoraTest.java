47ae1890a802abcbc9689030e9b3d52a4c1c76f9
GeorgeSalu <george.salu10@gmail.com>
2023-08-08 11:55:53 -0300

    projeto junit5-curso - verificando a igualdade

--- junit5-curso/src/test/java/CalculadoraTest.java
+++ junit5-curso/src/test/java/CalculadoraTest.java
@@ -7,5 +7,6 @@ public class CalculadoraTest {
 	public void somar() {
 		Calculadora calculadora = new Calculadora();
 		Assertions.assertTrue(calculadora.soma(2, 7) == 9);
+		Assertions.assertEquals(5, calculadora.soma(2, 3));
 	}
 }
