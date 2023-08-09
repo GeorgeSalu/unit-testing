2078b75a1c2b9cebdb1387c36916e96b5d3822a6
GeorgeSalu <george.salu10@gmail.com>
2023-08-09 19:37:16 -0300

    projeto junit5-curso - camada de servico

---
+++ junit5-curso/src/main/java/br/com/barriga/service/UsuarioService.java
@@ -0,0 +1,15 @@
+package br.com.barriga.service;
+
+import br.com.barriga.domain.Usuario;
+
+public class UsuarioService {
+
+	public Usuario salvar(Usuario usuario) {
+		// conectar com o banco 
+		// preparar sql
+		// executar a consulta 
+		// obter o usuario persistido
+		return usuario;
+	}
+	
+}
