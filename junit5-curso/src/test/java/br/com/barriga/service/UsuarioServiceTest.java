e26048ba1ba66a1bea82e1ee53cef14b3ac4740e
GeorgeSalu <george.salu10@gmail.com>
2023-08-12 14:44:14 -0300

    projeto junit5-curso - deve rejeitar usuario inexistente

--- junit5-curso/src/test/java/br/com/barriga/service/UsuarioServiceTest.java
+++ junit5-curso/src/test/java/br/com/barriga/service/UsuarioServiceTest.java
@@ -13,10 +13,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.barriga.domain.Usuario;
 import br.com.barriga.domain.builders.UsuarioBuilder;
+import br.com.barriga.domain.exceptions.ValidationException;
 import br.com.barriga.service.repository.UsuarioRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -68,7 +70,26 @@ public class UsuarioServiceTest {
 		verify(repository).salvar(userToSave);
 	}
 	
+	@Test
+	public void deveRejeitarUsuarioExistente() {
+		Usuario userToSave = umUsuario().comId(null).agora();
+		
+		when(repository.getUserByEmail(userToSave.email())).thenReturn(Optional.of(umUsuario().agora()));
+		
+		
+		ValidationException ex = Assertions.assertThrows(ValidationException.class, () -> service.salvar(userToSave));
+		
+		Assertions.assertTrue(ex.getMessage().endsWith("ja cadastrado!"));
+		
+		verify(repository, Mockito.never()).salvar(userToSave);
+	}
 }
+
+
+
+
+
+
 
 
 
