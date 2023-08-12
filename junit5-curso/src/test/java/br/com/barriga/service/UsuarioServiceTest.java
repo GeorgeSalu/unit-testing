1ece3fc3ba3df04cd8b992ea83b235d0ae6d0c35
GeorgeSalu <george.salu10@gmail.com>
2023-08-12 13:53:07 -0300

    projeto junit5-curso - deve salvar usuario

--- junit5-curso/src/test/java/br/com/barriga/service/UsuarioServiceTest.java
+++ junit5-curso/src/test/java/br/com/barriga/service/UsuarioServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.barriga.service;
 
+import static br.com.barriga.domain.builders.UsuarioBuilder.umUsuario;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -46,4 +47,29 @@ public class UsuarioServiceTest {
 		verifyNoMoreInteractions(repository);
 	}
 	
+	@Test
+	public void deveSalvarUsuarioComSucesso() {
+		UsuarioRepository repository = mock(UsuarioRepository.class);
+		service = new UsuarioService(repository);
+		
+		Usuario userToSave = umUsuario().comId(null).agora();
+		
+		when(repository.getUsarioByEmail(userToSave.email()))
+			.thenReturn(Optional.empty());
+		when(repository.salvar(userToSave))
+			.thenReturn(umUsuario().agora());
+		
+		
+		Usuario savedUser = service.salvar(userToSave);
+		
+		Assertions.assertNotNull(savedUser.id());
+		
+		verify(repository).getUsarioByEmail(userToSave.email());
+		verify(repository).salvar(userToSave);
+	}
+	
 }
+
+
+
+
