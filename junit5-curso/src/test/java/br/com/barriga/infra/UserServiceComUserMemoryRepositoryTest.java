df8231afaf5f6c14542f833987e3115f74b1b244
GeorgeSalu <george.salu10@gmail.com>
2023-08-12 10:39:25 -0300

    projeto junit5-curso - ordem de execucao

--- junit5-curso/src/test/java/br/com/barriga/infra/UserServiceComUserMemoryRepositoryTest.java
+++ junit5-curso/src/test/java/br/com/barriga/infra/UserServiceComUserMemoryRepositoryTest.java
@@ -1,30 +1,36 @@
 package br.com.barriga.infra;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import br.com.barriga.domain.Usuario;
 import br.com.barriga.domain.builders.UsuarioBuilder;
 import br.com.barriga.domain.exceptions.ValidationException;
 import br.com.barriga.service.UsuarioService;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserServiceComUserMemoryRepositoryTest {
 
 	private static UsuarioService service = new UsuarioService(new UsuarioMemoryRepository());
 	
 	@Test
+	@Order(1)
 	public void deveSalvarUsuarioValido() {
 		Usuario user = service.salvar(UsuarioBuilder.umUsuario().comId(null).agora());
 		Assertions.assertNotNull(user.id());
 	}
 	
 	@Test
+	@Order(2)
 	public void deveRejeitarUsuarioExistiente() {
 		ValidationException ex = Assertions.assertThrows(ValidationException.class, () -> {
 			service.salvar(UsuarioBuilder.umUsuario().comId(null).agora());
 		});
 		
-		
+		Assertions.assertEquals("Usuario user@gmail.com ja cadastrado!", ex.getMessage());
 	}
 	
 }
