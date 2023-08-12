00c3f6d277a540da973c7a2d69b00aa07c696510
GeorgeSalu <george.salu10@gmail.com>
2023-08-12 11:23:47 -0300

    projeto junit5-curso - deve retornar usuario por email

--- junit5-curso/src/test/java/br/com/barriga/service/UsuarioServiceTest.java
+++ junit5-curso/src/test/java/br/com/barriga/service/UsuarioServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import br.com.barriga.domain.Usuario;
+import br.com.barriga.domain.builders.UsuarioBuilder;
 import br.com.barriga.service.repository.UsuarioRepository;
 
 public class UsuarioServiceTest {
@@ -14,12 +15,26 @@ public class UsuarioServiceTest {
 	private UsuarioService service;
 	
 	@Test
-	public void deveRetornarUsaurioPorEmail() {
+	public void deveRetornarEmptyQuandoUsuarioInexistente() {
 		UsuarioRepository repository = Mockito.mock(UsuarioRepository.class);
 		service = new UsuarioService(repository);
 		
+		Mockito.when(repository.getUsarioByEmail("mail@mail.com")).thenReturn(Optional.empty());
+		
 		Optional<Usuario> user = service.getUserByEmail("mail@mail.com");
 		Assertions.assertTrue(user.isEmpty());
+	}
+	
+	@Test
+	public void deveRetornarUsuarioPorEmail() {
+		UsuarioRepository repository = Mockito.mock(UsuarioRepository.class);
+		service = new UsuarioService(repository);
+		
+		Mockito.when(repository.getUsarioByEmail("mail@mail.com"))
+				.thenReturn(Optional.of(UsuarioBuilder.umUsuario().agora()));
+		
+		Optional<Usuario> user = service.getUserByEmail("mail@mail.com");
+		Assertions.assertFalse(user.isEmpty());
 	}
 	
 }
