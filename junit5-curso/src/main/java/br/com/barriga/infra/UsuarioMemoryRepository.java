c437a0660e7be2e48932a07aad8dccf93a129c34
GeorgeSalu <george.salu10@gmail.com>
2023-08-12 10:23:58 -0300

    projeto junit5-curso - memory data

---
+++ junit5-curso/src/main/java/br/com/barriga/infra/UsuarioMemoryRepository.java
@@ -0,0 +1,43 @@
+package br.com.barriga.infra;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import br.com.barriga.domain.Usuario;
+import br.com.barriga.service.repository.UsuarioRepository;
+
+public class UsuarioMemoryRepository implements UsuarioRepository {
+	
+	private List<Usuario> users;
+	private Long currentId;
+	
+	public UsuarioMemoryRepository() {
+		currentId = 0L;
+		users = new ArrayList<>();
+		salvar(new Usuario(null, "User #1", "user@email.com", "123456"));
+	}
+
+	@Override
+	public Usuario salvar(Usuario usuario) {
+		Usuario newUser = new Usuario(nextId(), usuario.nome(), usuario.email(), usuario.senha());
+		users.add(newUser);
+		return newUser;
+	}
+
+	@Override
+	public Optional<Usuario> getUsarioByEmail(String email) {
+		return users.stream()
+					.filter(user -> user.email().equalsIgnoreCase(email))
+					.findFirst();
+	}
+
+	public void printUsers() {
+		System.out.println(users);
+	}
+	
+	private Long nextId() {
+		return ++currentId;
+	}
+	
+}
